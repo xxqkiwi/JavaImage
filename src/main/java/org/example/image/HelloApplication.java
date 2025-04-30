1ffa05369f6df98d45b5eaaf980c15b289938b77
fll <2720139445@qq.com>
2025-04-30 10:19:52 +0800

    test

--- src/main/java/org/example/image/HelloApplication.java
+++ src/main/java/org/example/image/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/org/example/image/Index/index.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/org/example/image/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 620, 540);
         stage.setTitle("美图看看");
         stage.setScene(scene);
